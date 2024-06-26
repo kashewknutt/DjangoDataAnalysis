1ea9506ae11e1ed0c49a9f2937dc437a189e0707
Kashew Knutt <kashewknutt@gmail.com>
2024-06-26 13:31:56 +0530

    bad

--- dashboard_project/dashboard_app/views.py
+++ dashboard_project/dashboard_app/views.py
@@ -3,7 +3,11 @@ from dashboard_app.models import DataEntry
 from django.shortcuts import render
 
 def get_data(request):
-    entries = DataEntry.objects.all().values()
+    #entries = DataEntry.objects.all().values()
+    entries = DataEntry.objects.all().values(
+        'topic', 'intensity', 'sector', 'insight', 'url', 'region', 'country', 'relevance', 'pestle', 'source', 'title', 'likelihood'
+    )
+
     return JsonResponse(list(entries), safe=False)
 
 def index(request):
